b208adac349c46034bf6ffca4643fec6c15e5b0d
M1noa <git@minoa.cat>
2026-07-14 22:06:39 -0500

    Implement MinecraftServer::getInstance() and getPlayers()

--- src/Minecraft.Client/net/minecraft/server/MinecraftServer.cpp
+++ src/Minecraft.Client/net/minecraft/server/MinecraftServer.cpp
@@ -1,1 +1,11 @@
 #include "net/minecraft/server/MinecraftServer.h"
+
+MinecraftServer* MinecraftServer::sInstance = nullptr;
+
+MinecraftServer* MinecraftServer::getInstance() {
+    return sInstance;
+}
+
+PlayerList* MinecraftServer::getPlayers() {
+    return m_players;
+}
